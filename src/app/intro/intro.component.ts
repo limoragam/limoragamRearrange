e7a53762d9048ae4de783c5712c5b7f1dc682754
limoragam <33860860+limoragam@users.noreply.github.com>
2018-01-11 00:11:47 +0200

    Rearrange 0

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { IntroService } from './intro.service';
 
@@ -15,41 +15,137 @@ import { IntroService } from './intro.service';
     ]),
   ]
 })
-export class IntroComponent implements AfterViewInit {
+export class IntroComponent implements AfterContentInit {
 
   currentImage = 'sketch';
   translateString = "0";
-  containerWidth = 0;
-  containerHeight = 0;
+  playgroundWidth = 0;
+  playgroundHeight = 0;
+  originalImageWidth = 0;
+  originalImageHeight = 0;
+  imageResizeFactor = 1;
+  rearrangeIndex = 0;
 
   constructor(public introService:IntroService) {}
 
-  ngAfterViewInit() {
-    this.onResize();
+  // Calculations and change of width/height which are actually bound to children
+  // should be done after content init.
+  // If it is later (AfterViewInit), there is an ExpressionChangedAfterItHasBeenCheckedError
+  // thrown about inconsistent values between the parent/child components (dev only)
+  ngAfterContentInit() {
+    this.originalDimensionsSet.then(()=>{
+        this.getPlaygroundDimensions();
+        this.translateImages();
+      }      
+    );
+  }
 
+  ngAfterViewInit() {
     setTimeout(()=>{
       this.currentImage = 'outline';
       setTimeout(()=>{
         this.currentImage = 'sigmar';
-      }, 2000);
-    },2000);
+        this.introService.clickStyle.opacity = 1;
+      }, 200); //1000
+    },200);  //3000
+  }
+
+  // Set original dimensions by Promise so that things can be done after dimensions are set
+  originalDimensionsSet = new Promise(
+    (resolve, reject)=>{
+      let image = new Image();
+      image.onload = ()=>{
+        this.originalImageHeight = image.height;
+        this.originalImageWidth = image.width;
+        resolve();
+      }
+      image.src = this.introService.images.sketchSrc;
+    }
+  );
+
+  getPlaygroundDimensions() {
+    this.playgroundWidth = $(".sketch").width();
+    this.playgroundHeight = $(".sketch").height();
+    this.imageResizeFactor = this.playgroundHeight/this.originalImageHeight;
   }
 
   translateImages() {
-    let image = new Image();
-    image.onload = ()=> {
-      // image height is the same as container height
-      // need to calc image width, subtract container width and divide by 2 to get translate
-      let imageWidth = this.containerHeight * image.width / image.height;
-      let offset = (imageWidth-this.containerWidth) / 2;
-      this.translateString = "translateX(-" + offset + "px)";
-    }
-    image.src = this.introService.images.sketch.src;
+    // image height is the same as container height
+    // need to calc image width, subtract container width and divide by 2 to get translate
+    let imageWidth = this.playgroundHeight * this.originalImageWidth / this.originalImageHeight;
+    let offset = (imageWidth-this.playgroundWidth) / 2;
+    this.translateString = "translateX(-" + offset + "px)";
+}
+
+  onResize(event:Event) {
+    this.getPlaygroundDimensions();
+    this.translateImages();
   }
 
-  onResize() {
-    this.containerWidth = $(".sketch").width();
-    this.containerHeight = $(".sketch").height();
-    this.translateImages();
+  onClick() {
+    switch(this.rearrangeIndex) {
+      case 0:
+        this.rearrange0();
+        break;
+
+      default:
+        console.log("No rearrange method found");
+        break;
+    }
+    this.rearrangeIndex++;
+  }
+
+  rearrange0() {
+    let letters = document.getElementsByTagName("path");
+
+    // Rearrange Limor Agam
+    let laYTranslation = letters[0].getBoundingClientRect().height * this.imageResizeFactor;
+    let laXTranslation = (letters[8].getBoundingClientRect().right - letters[0].getBoundingClientRect().left)/6*this.imageResizeFactor;
+
+    let limorTranslationString = "translateY(-" + laYTranslation + "px) translateX(" + laXTranslation + "px)";
+    let agamTranslationString = "translateX(-" + laXTranslation + "px)";
+
+    for(let i=0;i<5;++i) {
+      this.introService.sigmarLetters[i].style['transform'] = limorTranslationString;
+    }
+    
+    for(let i=5;i<9;++i) {
+      this.introService.sigmarLetters[i].style['transform'] = agamTranslationString;
+    }
+
+    // Rearrange Customized
+    let cusYTranslation = letters[9].getBoundingClientRect().height * this.imageResizeFactor;
+    let cusXTranslation = (letters[18].getBoundingClientRect().right - letters[9].getBoundingClientRect().left)/6*this.imageResizeFactor;
+
+    let custoTranslationString = "translateX(" + cusXTranslation + "px)";
+    let mizedTranslationString = "translateY(" + cusYTranslation + "px) translateX(-" + cusXTranslation + "px)";
+
+    for(let i=9;i<14;++i) {
+      this.introService.sigmarLetters[i].style['transform'] = custoTranslationString;
+    }
+    
+    for(let i=14;i<19;++i) {
+      this.introService.sigmarLetters[i].style['transform'] = mizedTranslationString;
+    }
+
+    this.introService.sigmarLetters[13].style['fill'] = "#88aa00";
+
+    // Rearrange Websites
+    let webYTranslation = letters[19].getBoundingClientRect().height * this.imageResizeFactor;
+    let webXTranslation = (letters[26].getBoundingClientRect().right - letters[19].getBoundingClientRect().left)/6*this.imageResizeFactor;
+
+    let webTranslationString = "translateY(" + webYTranslation + "px) translateX(" + cusXTranslation + "px)";
+    let sitesTranslationString = "translateY(" + (2*webYTranslation) + "px) translateX(-" + webXTranslation + "px)";
+
+    for(let i=19;i<22;++i) {
+      this.introService.sigmarLetters[i].style['transform'] = webTranslationString;
+    }
+    
+    for(let i=22;i<27;++i) {
+      this.introService.sigmarLetters[i].style['transform'] = sitesTranslationString;
+    }
+
+    this.introService.clickStyle['top'] = "10vh";
+
   }
 }
